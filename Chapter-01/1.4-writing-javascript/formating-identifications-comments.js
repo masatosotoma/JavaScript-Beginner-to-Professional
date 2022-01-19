3203128fd787672950ffb45286d64c64b5b7382b
masatosotoma <masatosotoma@gmail.com>
2022-01-18 20:15:20 -0500

    Formatting and Comments

---
+++ Chapter-01/1.4-writing-javascript/formating-identifications-comments.js
@@ -0,0 +1,20 @@
+let status = "new";
+let scared = true;
+if (status === true) {
+  console.log("Welcome to JavaScript!");
+  if (scared) {
+    console.log("Don't worry you will be fine!");
+  } else {
+    console.log("You're brave! You are going to do great!");
+  }
+} else {
+  console.log("Welcome back, I knew you'd like it!");
+}
+
+//I'm a single Line comment
+//console.log("single line comment, not logged");
+
+/* I'm a multi-line comment. Whatever is between the slash asterisk and
+the asterisk slash will not get executed.
+console.log("I'm not logged, because I'm a comment");
+*/
