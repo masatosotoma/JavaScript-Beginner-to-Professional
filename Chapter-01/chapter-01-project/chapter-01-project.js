aa2dc563ea4108ee6390eb82420a8fbdf5338905
masatosotoma <masatosotoma@gmail.com>
2022-01-18 20:57:40 -0500

    Chapter 01 Project Completed

---
+++ Chapter-01/chapter-01-project/chapter-01-project.js
@@ -0,0 +1,7 @@
+console.log("My name is Masato Sotoma");
+
+/*
+I output my full name on the console
+"My name is Masato Sotoma"
+This Javascript file is also connected to HTML file "chapter-01-project"
+*/
