8a86e62e15659f83308dd40142ace1f63065bb8a
masatosotoma <masatosotoma@gmail.com>
2022-01-18 20:48:25 -0500

    Random number function

---
+++ Chapter-01/1.4-writing-javascript/random-number.js
@@ -0,0 +1,8 @@
+Math.random();
+console.log(Math.random()); //Get random number from 0 - 1
+
+Math.random();
+console.log(Math.random() * 100); //Get random number from 0 - 100
+
+Math.random();
+console.log(Math.floor(Math.random() * 100)); //Get random number from 0 with no decimal result
