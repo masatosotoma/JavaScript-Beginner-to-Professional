d9e77c025614e94d8e89b3de3fb620813bc1fc22
masatosotoma <masatosotoma@gmail.com>
2022-01-18 20:32:40 -0500

    Practice exercise 1.4 to add comments

--- Chapter-01/1.4-writing-javascript/practice-exercise-formatting-and-comment.js
+++ Chapter-01/1.4-writing-javascript/practice-exercise-formatting-and-comment.js
@@ -1,4 +1,8 @@
-let a = 10; //To set a value of 10
-console.log(a);
-/*Print the valuable "a" 
-to the web console*/
+let a = 10;  //To set a value of 10
+console.log(a); //Print the valuable "a" to the web console
+
+/* 
+Practice to set the value
+1.To set value of 10
+2.Print the value on the console to check if the value is assigned without problems
+*/
