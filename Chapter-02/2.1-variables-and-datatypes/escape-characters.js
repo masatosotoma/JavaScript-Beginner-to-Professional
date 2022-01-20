6f8dd26be19c66836db6d30958a4ecea3d8a0463
masatosotoma <masatosotoma@gmail.com>
2022-01-19 22:53:23 -0500

    Express many characters for strings

---
+++ Chapter-02/2.1-variables-and-datatypes/escape-characters.js
@@ -0,0 +1,8 @@
+let str = 'Hello, what\'s your name? Is it "Mike"?'; //backslash to express double quates
+let str2 = 'Hello, what\'s your name? Is it "Mike"?'; //backslash to express a signle quate and singles quotes for double quates
+let str3 = "New \nline."; //backslash n to go to the next line
+let str4 = "I'm containing a backslash: \\!"; //double backslash to express a single slash
+console.log(str);
+console.log(str2);
+console.log(str3);
+console.log(str4);
