73206a68ebd6615de14d7c65950c728fd047314a
masatosotoma <masatosotoma@gmail.com>
2022-01-20 19:35:03 -0500

    number data types

---
+++ Chapter-02/2.1-variables-and-datatypes/bigint.js
@@ -0,0 +1,3 @@
+let bigNr = 90071992547409920n; // A BigInt data type can be recognized by the postfix n
+
+// You cannot mix BigInt with the Number data type to perform operations
---
+++ Chapter-02/2.1-variables-and-datatypes/numbers.js
@@ -0,0 +1,13 @@
+let intNr = 1;
+let decNr = 1.5; // decimals
+let expNr = 1.4e15; // exponentials
+let octNr = 0o10; // octal,decimal version would be 8
+let hexNr = 0x3E8; // hexadecimal, decimal version would be 1000
+let binNr = 0b101; // binary, decimal version would be 5
+
+let intNr2 = 3434;
+let intNr3 = -111; // negative number
+
+let decNr2 = 45.78; // floating numbers
+
+let binNr2 = 0b100; // binary numbers
