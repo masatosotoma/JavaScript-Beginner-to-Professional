4071f4bf9d9b22b9071c48a01107437ef8c98696
masatosotoma <masatosotoma@gmail.com>
2022-01-20 20:40:21 -0500

    More variables, such as boolean symbols and null

--- Chapter-02/2.1-variables-and-datatypes/symbol.js
+++ Chapter-02/2.1-variables-and-datatypes/symbol.js
@@ -5,5 +5,5 @@ let str2 = "JavaScript is fun!";
 let sym1 = Symbol("Java Script is fun!");
 let sym2 = Symbol("Java Script is fun!");
 
-console.log("There two strings are the same:", str1 === str2);
-console.log("There two strings are the same:", sym1 === sym2);
+console.log("There two strings are the same:", str1 === str2); //Return true because they are the same strings
+console.log("There two strings are the same:", sym1 === sym2); //Return false because they are unique symbol variables
