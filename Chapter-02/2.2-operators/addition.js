35cbb8a6fec8da42e4aad8932a610749ada406b8
masatosotoma <masatosotoma@gmail.com>
2022-01-23 16:55:00 -0500

    Variables and Operaters

---
+++ Chapter-02/2.2-operators/addition.js
@@ -0,0 +1,11 @@
+let nr1 = 12;
+let nr2 = 14;
+let result1 = nr1 + nr2;
+
+console.log(result1);
+
+let str1 = "Hello ";
+let str2 = "addition";
+let result2 = str1 + str2; // will concatenate two strings
+
+console.log(result2);
