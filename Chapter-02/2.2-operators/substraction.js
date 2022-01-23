19af42a4b786d8ac54269bf1c48886b9d7628d52
masatosotoma <masatosotoma@gmail.com>
2022-01-23 17:08:38 -0500

    Substraction

---
+++ Chapter-02/2.2-operators/substraction.js
@@ -0,0 +1,9 @@
+let nr1 = 20;
+let nr2 = 4;
+let str1 = "Hi";
+let nr3 = 3;
+
+let result1 = nr1 - nr2;
+let result2 = str1 - nr3; // You can't substract number from strings type so it return NaN
+
+console.log(result1, result2);
