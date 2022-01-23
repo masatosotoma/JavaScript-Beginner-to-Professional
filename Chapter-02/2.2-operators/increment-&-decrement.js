a7bd4e75dab1474a3af197719aacde371734dbcf
masatosotoma <masatosotoma@gmail.com>
2022-01-23 17:32:19 -0500

    Unary operators: Operands increment and decrement

---
+++ Chapter-02/2.2-operators/increment-&-decrement.js
@@ -0,0 +1,9 @@
+//Operand:  the part of a computer instruction which specifies what data is to be manipulated or operated on
+
+let nr1 = 4;
+nr1++; // incrementation, which change value +1 to the value of nr1
+console.log(nr1);
+
+let nr2 = 4;
+nr2--; // decrementation, which change value -1 to the value of nr1
+console.log(nr2);
