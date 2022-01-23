ea029b710f96eac15c47d364738fc52710806f67
masatosotoma <masatosotoma@gmail.com>
2022-01-23 17:12:20 -0500

    Multiplication.js

--- Chapter-02/2.2-operators/Multiplication.js
+++ Chapter-02/2.2-operators/Multiplication.js
@@ -4,6 +4,6 @@ let str1 = "Hi";
 let nr3 = 3;
 
 let result1 = nr1 * nr2;
-let result2 = str1 * nr3;
+let result2 = str1 * nr3; // You can't multiply numbers and strings type so it return NaN
 
 console.log(result1, result2);
