5fa6121969f1be26f81a0535b2c9c302f9236573
masatosotoma <masatosotoma@gmail.com>
2022-01-23 17:04:52 -0500

    Practice for addition operator

---
+++ Chapter-02/2.2-operators/practice-exercise-2.2.js
@@ -0,0 +1,14 @@
+let myName = "Masato Sotoma";
+let myAge = 31;
+let codeJavaScript = true;
+
+let sentence1 =
+  "Hello, my name is " +
+  myName +
+  ", I am " +
+  myAge +
+  " yeas old and I can code JavaScript:" +
+  codeJavaScript +
+  ".";
+
+console.log(sentence1);
