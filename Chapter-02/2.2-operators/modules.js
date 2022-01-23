5de1c94212a16794c5f4cd5673e0be46caf1bde0
masatosotoma <masatosotoma@gmail.com>
2022-01-23 17:27:00 -0500

    Modules

---
+++ Chapter-02/2.2-operators/modules.js
@@ -0,0 +1,17 @@
+// Modulus: you determine how much is left after dividing a number by another number in its entirely
+
+let nr1 = 10;
+let nr2 = 3;
+let result1 = nr1 % nr2;
+
+console.log(`${nr1} % ${nr2} = ${result1}`);
+
+let nr3 = 8;
+let nr4 = 2;
+let result2 = nr3 % nr4;
+console.log(`${nr3} % ${nr4} = ${result2}`);
+
+let nr5 = 15;
+let nr6 = 4;
+let result3 = nr5 % nr6;
+console.log(`${nr5} % ${nr6} = ${result3}`);
