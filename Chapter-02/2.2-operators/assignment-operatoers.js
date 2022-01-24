22472cc08cbe51eee6286214c6b352aeacc04e6a
masatosotoma <masatosotoma@gmail.com>
2022-01-23 22:23:52 -0500

    Studying various Assignment operators

---
+++ Chapter-02/2.2-operators/assignment-operatoers.js
@@ -0,0 +1,29 @@
+let x = 2;
+x += 2; // means x = x+2
+
+console.log(x);
+
+let y = 4;
+y -= 2; // means y = x-2
+
+console.log(y);
+
+let z = 3;
+z *= 4; // means z = z*4
+
+console.log(z);
+
+let a = 10;
+a /= 2; // means a = a/2
+
+console.log(a);
+
+let b = 2;
+b **= 2; // means b = b*b = (2*2)
+
+console.log(b);
+
+let c = 10;
+c %= 3; // means to return the module of 3/10
+
+console.log(c);
