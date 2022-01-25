0e0a705d5272bd253072f9e56fe8737ded9aed93
masatosotoma <masatosotoma@gmail.com>
2022-01-24 21:08:45 -0500

    Comparison operators

---
+++ Chapter-02/2.3-comparison-operators/not-equal.js
@@ -0,0 +1,6 @@
+let x = 5;
+let y = "5";
+
+console.log(x != y); // != means to check if two values are not the same in terms of value not data types.
+
+console.log(x !== y); // to check if two values are not the same both values and data types.
