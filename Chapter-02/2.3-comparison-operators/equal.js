4f9480856adb2b2b7fdc3bb8bf17b61bb274de60
masatosotoma <masatosotoma@gmail.com>
2022-01-24 21:10:09 -0500

    Comparison operators

--- Chapter-02/2.3-comparison-operators/equal.js
+++ Chapter-02/2.3-comparison-operators/equal.js
@@ -1,6 +1,6 @@
 let x = 5;
 let y = "5";
 
-console.log(x == y); // two equals means to check for equal values and not for data type.
+console.log(x == y); // two equals means to check for equal values and not for data type. Will return true or false.
 
 console.log(x === y); // strict equality. Triple equals means to check if the values are equals both values and data types.
