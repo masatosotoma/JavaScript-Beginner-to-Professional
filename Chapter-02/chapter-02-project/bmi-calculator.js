350707708625412eca7f667ecda9e6ca17c8f2e0
masatosotoma <masatosotoma@gmail.com>
2022-01-25 15:01:55 -0500

    Chaptor2 Projects

---
+++ Chapter-02/chapter-02-project/bmi-calculator.js
@@ -0,0 +1,11 @@
+let heightInInch = 63;
+let weightInPound = 130;
+
+let heightInCm = heightInInch * 2.54;
+let weightInKg = weightInPound / 2.2046;
+
+let bmi = weightInKg / (heightInCm / 100) ** 2;
+
+console.log("Height(in cm):" + heightInCm);
+console.log("weight(in kg):" + weightInKg);
+console.log("BMI:" + bmi);
---
+++ Chapter-02/chapter-02-project/miles-to-km-converter.js
@@ -0,0 +1,4 @@
+let totalMiles = 130;
+let mileToKm = totalMiles * 1.60934;
+
+console.log("The distance of " + totalMiles + "km is equal to " + mileToKm + "miles.");
