7ad5c66763a4c71e40e10792fbbd662fc271bade
masatosotoma <masatosotoma@gmail.com>
2022-01-31 23:31:50 -0500

    Chapter exercise 3.1

---
+++ Chapter-03/3.1-arrays-and-properties/practice-exercise-3.1.js
@@ -0,0 +1,7 @@
+shoppingList = ["Milk", "Bread", "Apples"];
+
+console.log(shoppingList.length); // check the list length
+
+shoppingList[1] = "Banana"; // update the value in the array
+
+console.log("The shopping List :", shoppingList);
