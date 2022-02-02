f795d9508f8c88837fb4137734a4c9050d8defa5
masatosotoma <masatosotoma@gmail.com>
2022-02-01 22:29:22 -0500

    Sorting and reversing arrays

---
+++ Chapter-03/3.2-array-methods/reversing.js
@@ -0,0 +1,4 @@
+//reverse() method to change the order of array to reverse
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+names.reverse(); // to change the order reversed
+console.log(names);
---
+++ Chapter-03/3.2-array-methods/sorting.js
@@ -0,0 +1,8 @@
+//sort() method to sort numbers from small to high and strings A-Z
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+names.sort(); // return A-Z sorted array 
+console.log(names);
+
+let ages = [18, 72, 33, 56, 40];
+ages.sort(); // return small to high sorted array
+console.log(ages);
