dc164c971bedafd049741de66b2526e3367e663a
masatosotoma <masatosotoma@gmail.com>
2022-02-04 22:46:04 -0500

    If and if-else statement + practice

---
+++ Chapter-04/if-and-if-else-statement/if-and-if-else-statement.js
@@ -0,0 +1,29 @@
+/* if statement = 
+if *some condition is true*, then *a certain action will happen*, else *another action will happen*
+*/
+if (expression) {
+  // code associated with the if block
+  // will only be executed if the expression is true
+} else {
+  // code associated with the else block
+  // we don't need an else block, it is optional
+  // this code will only be executed if the expression is false
+}
+
+// Below an example
+let rain = true;
+
+if (rain) {
+  console.log("** Taking my umbrella when I need to go outside **");
+} else {
+  console.log("** I can leave my umbrella at home **");
+}
+// Since it's true, it will return the first statement
+
+//Another example using operator
+
+if (age < 18) {
+  console.log("We're very sorry, but you can't get in under 18");
+} else {
+  console.log("Welcome!");
+}
---
+++ Chapter-04/if-and-if-else-statement/practice-exercise-4.1.js
@@ -0,0 +1,10 @@
+// create a variable with a boorean value
+let myVariable = true;
+
+// output the variable in the console
+console.log(myVariable);
+
+// use if statement to output the message in the console
+if (myVariable) {
+  console.log("the variable is true!");
+}
