76bb67dc767be44acd87826e3c7494609b57d4ac
masatosotoma <masatosotoma@gmail.com>
2022-02-06 11:03:06 -0500

    Conditional ternary operator practice exercise 4.3

---
+++ Chapter-04/conditional-ternary-operators/practice-exercise-4.3.js
@@ -0,0 +1,5 @@
+let yourId = true; // to create a boolean value for your ID
+// create a message that will check if your id is valid or not
+let message = (yourId) ? "It's valid" : "It's invalid";
+
+console.log(message); // output to the console
