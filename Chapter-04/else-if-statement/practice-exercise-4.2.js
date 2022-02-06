d72cb90bf16808b2766c03b86de5d40925710079
masatosotoma <masatosotoma@gmail.com>
2022-02-06 10:43:51 -0500

    Practice exercise 4.2 for If and If else statement

---
+++ Chapter-04/else-if-statement/practice-exercise-4.2.js
@@ -0,0 +1,16 @@
+let age = prompt("How old are you?"); // to create a prompt to ask your age
+age = Number(age); // convert the response to number datatype
+let message; // declare a variable "message"
+
+/* 
+create if statement that message will tell you people older than or equal to 21 can enter and drink alcohol,
+people older than or equal to 19 can enter but not drink, otherwise you are not allowed to enter.
+*/
+if (age >= 21) {
+  message = "Allowed the entry and to purchase alcohol";
+} else if (age >= 19) {
+  message = "Allowed the entry but not to purchase alcohol";
+} else {
+  message = "Not allowed to enter";
+}
+console.log(message);
