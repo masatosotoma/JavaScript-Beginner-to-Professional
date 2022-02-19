6f28133b386291f895653f050d0a1c6ce75e577c
masatosotoma <masatosotoma@gmail.com>
2022-02-18 22:20:01 -0500

    Practiced exercise 5.4 Table generator

--- Chapter-05/5.3-for-loops/practice-exercise-5.4.js
+++ Chapter-05/5.3-for-loops/practice-exercise-5.4.js
@@ -7,10 +7,10 @@ let columns = 10; // to set a number of columns
 
 let counter = 0;
 
-for (i = 0; i <= rows; i++) {
+for (i = 0; i <= rows; i++) { // to creating rows with for loops
   let tempTable = [];
-  for (let x = 0; x <= columns; x++) {
-    counter++;
+  for (let x = 0; x <= columns; x++) { // to create columns with nested for loops
+    counter++; // adding numbers of the tables
     tempTable.push(counter);
   }
   myTable.push(tempTable);
