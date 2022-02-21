dabac1f9feb1f19970f6311ee6d905f5f14ccffe
masatosotoma <masatosotoma@gmail.com>
2022-02-21 17:31:54 -0500

    Practice exercise loop using continue and break

---
+++ Chapter-05/5.5-loops-&-objects-for-in-loop/practice-exercise-5.8.js
@@ -0,0 +1,24 @@
+let output = ""; // to create a string value for output
+let skipNum = 8; // set the number that will be skipped
+
+// create a for loop counting 10 that skips the numbe 8 using continue keyword
+for (let i = 0; i < 10; i++) {
+  if (i === skipNum) { // to make the loop skip the numbet set before
+    continue;
+  }
+  output += i;
+}
+console.log(output);
+
+
+// create a for loop counting 10 that ends the numbe 8 using break keyword
+let output = ""; //
+let skipNum = 8;
+
+for (let i = 0; i < 10; i++) {
+  if (i === skipNum) {
+    break;
+  }
+  output += i;
+}
+console.log(output);
