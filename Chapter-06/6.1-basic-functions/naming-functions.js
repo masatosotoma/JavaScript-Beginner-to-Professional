4c37e5891702057389f9f73162a1880fe7db477a
masatosotoma <masatosotoma@gmail.com>
2022-02-22 21:19:17 -0500

    Learning how to name functions

---
+++ Chapter-06/6.1-basic-functions/naming-functions.js
@@ -0,0 +1,20 @@
+/*
+How you give your function a name is important
+there are best practices to keep in mind. 
+TO KEEP IT SHORT
+*/
+
+// Camelcase your functions: 1st word lowercase,and new words start with capitals
+let camelCase = function () {
+  console.log("camelCase");
+};
+
+// Make sure that the name describes what the function is doing
+let addNumbers = function () {
+  num++;
+};
+
+// Use a verb to describe what the function is doing
+let sayHi = function () {
+  console.log("Hi!");
+};
