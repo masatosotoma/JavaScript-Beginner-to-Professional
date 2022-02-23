e4f0921c082e14806c5e701c8958a961fce33135
masatosotoma <masatosotoma@gmail.com>
2022-02-22 20:33:02 -0500

    Explained to invoke functions

---
+++ Chapter-06/6.1-basic-functions/invoking-functions.js
@@ -0,0 +1,5 @@
+// You can recognize functions by the parentheses at the end.
+// You can invoke the functions like below
+
+nameOfTheFunction(); // invoking the function called the name of function
+functionThatTakesInput("the input", 5, true); // to call functions with 3 required arguments
