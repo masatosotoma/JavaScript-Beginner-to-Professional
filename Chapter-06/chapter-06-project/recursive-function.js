5818a468245ac9bf852987394a4d4cf973b86da3
masatosotoma <masatosotoma@gmail.com>
2022-03-06 13:54:15 -0500

    Created a recursive counter

---
+++ Chapter-06/chapter-06-project/recursive-function.js
@@ -0,0 +1,10 @@
+
+//Create a recursive function that counts up to 10
+let recursive = function counter(i) {
+  console.log(i);
+  if (i < 10) {
+    return counter(i + 1);
+  }
+  return;
+};
+recursive(20);
