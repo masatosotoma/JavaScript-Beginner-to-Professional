041eaf6ed67253ca7f34d324e4587fac8a1d4b65
masatosotoma <masatosotoma@gmail.com>
2022-03-13 15:42:18 -0400

    Explained global methods

---
+++ Chapter-08/8.1-global-methods/global-methods.js
@@ -0,0 +1,7 @@
+
+let x = 7;
+console.log(Number.isNaN(x)); // to check if the value is not a number. If it's not, return true
+
+console.log(isNaN(x)); //You can write like this in short way
+
+console.log(Math.floor(Math.random()*100)); 
