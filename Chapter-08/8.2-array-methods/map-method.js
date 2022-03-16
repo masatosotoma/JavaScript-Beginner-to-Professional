12b9aaef85cbb9ac78c3512a9ea0b99bb61ee78b
masatosotoma <masatosotoma@gmail.com>
2022-03-15 22:35:11 -0400

    Explained map method

---
+++ Chapter-08/8.2-array-methods/map-method.js
@@ -0,0 +1,6 @@
+// map() = return a new array with all the new values. You need to specify how to create these new values
+// creating the new values can be done with the arrow function
+
+let arr = [1, 2, 3, 4];
+let mapped_arr = arr.map(x => x + 1); // each of the original array values has been increased by 1
+console.log(mapped_arr);
