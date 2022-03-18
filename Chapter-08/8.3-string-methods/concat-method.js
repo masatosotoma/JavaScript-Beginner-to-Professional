ad2da54f9887418ac15b9374a3c1702f891f22aa
masatosotoma <masatosotoma@gmail.com>
2022-03-17 20:39:18 -0400

    Reviewed concat() method

---
+++ Chapter-08/8.3-string-methods/concat-method.js
@@ -0,0 +1,15 @@
+
+// concat() = to concatenate strings
+
+//example
+let s1 = "Hello ";
+let s2 = "JavaScript";
+
+let result = s1.concat(s2);
+
+console.log(result);
+
+let s3 = " is fun!";
+let result2 = s1.concat(s2.concat(s3));
+
+console.log(result2);
