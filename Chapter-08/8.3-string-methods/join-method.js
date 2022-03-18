b895b92dee995830bf2c19f4cd0b4f6c3abb97e6
masatosotoma <masatosotoma@gmail.com>
2022-03-17 21:10:15 -0400

    Reviewed join method

---
+++ Chapter-08/8.3-string-methods/join-method.js
@@ -0,0 +1,13 @@
+// join() method = to convert an array to a string
+
+// example
+
+let letters = ["a", "b", "c"];
+let x = letters.join();
+console.log(x);
+
+//  If you want something else other than a comma for dividing, you can specify with parameter
+
+let letters2 = ["a", "b", "c"];
+let y = letters.join("-");
+console.log(y);
