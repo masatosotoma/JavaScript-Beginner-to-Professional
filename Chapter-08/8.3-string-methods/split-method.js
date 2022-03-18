87cfd797d7e2dc8b63d79213161126af1b222f4e
masatosotoma <masatosotoma@gmail.com>
2022-03-17 20:46:57 -0400

    Reviewed split method

---
+++ Chapter-08/8.3-string-methods/split-method.js
@@ -0,0 +1,14 @@
+// split() = to convert a string to an array. It's not a change of the original string, but make a new array
+// You have to tell the split method on what string it should split
+
+//example
+let result = "Hello JavaScript";
+let arr_result = result.split(" "); // split by every space
+
+console.log(arr_result);
+
+//example2
+let favauriteFruits = "strawberry,watermelon,grapefruit";
+let arr_fruits = favauriteFruits.split(","); // split by every ,
+
+console.log(arr_fruits);
