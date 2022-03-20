58a6bfb773b19f611bf4901f38fdfc9a91b91a5e
masatosotoma <masatosotoma@gmail.com>
2022-03-20 19:22:28 -0400

    Reviewed isNaN method

---
+++ Chapter-08/8.4-number-methods/isNAN-method.js
@@ -0,0 +1,12 @@
+// isNaN() = to check if the variable is number
+
+let x = 34;
+console.log(isNaN(x)); // to check if variable is a number
+console.log(!isNaN(x)); // to check if variable is not a number
+let str = "hi";
+console.log(isNaN(str));
+
+// If it's number but string value, it will return false
+
+let str1 = "5";
+console.log(isNaN(str1));
