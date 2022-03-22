985a5c8e837d5d5e1c47b063a9e159c21d6e1dc6
masatosotoma <masatosotoma@gmail.com>
2022-03-22 10:57:53 -0400

    Explained sqrt() and pow() methods

---
+++ Chapter-08/8.5-math-methods/square-root-&-power-of-methods.js
@@ -0,0 +1,8 @@
+// Math.sqrt() = to use to calculate the square root of a certain number
+
+let result = Math.sqrt(64);
+console.log(result); // it loggs 8
+
+// Math.pow(base,exponent) = to raise a number to a certain power
+let result2 = Math.pow(5, 3); // it means 5 * 5 * 5
+console.log(result2); //it loggs 125
