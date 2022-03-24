2f9a817b798a226105a39901d4ddf773f6eb18ba
masatosotoma <masatosotoma@gmail.com>
2022-03-24 19:34:05 -0400

    Explained to convert dates to strings

---
+++ Chapter-08/8.6-date-methods/convert-date-to-string.js
@@ -0,0 +1,7 @@
+// toDateString() = to convert dates back to strings
+
+let d = new Date();
+console.log(d.toDateString());
+
+// toLocaleDateString() = another method to convert dates to string with different format
+console.log(d.toLocaleDateString());
