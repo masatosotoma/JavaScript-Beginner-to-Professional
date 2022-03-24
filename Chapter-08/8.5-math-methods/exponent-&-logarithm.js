0271f37e81cb979e8dbb739d0d6b8e955579ca58
masatosotoma <masatosotoma@gmail.com>
2022-03-23 20:53:38 -0400

    Explained expo() and log() methods

---
+++ Chapter-08/8.5-math-methods/exponent-&-logarithm.js
@@ -0,0 +1,10 @@
+// Math.expo() = to return the number to which e must be raised to get the input
+
+let x = 2;
+let exp = Math.exp(x);
+console.log("Exp:", exp);
+
+// Math.log() = to calculate the natural logarithm
+
+let log = Math.log(exp); //it returns 7.3890---
+console.log("Log:", log); // it return 2
