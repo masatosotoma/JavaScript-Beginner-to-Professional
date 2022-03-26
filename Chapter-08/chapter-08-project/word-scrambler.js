15ae70067bda5f526ae8856410283f2e938c3cc0
masatosotoma <masatosotoma@gmail.com>
2022-03-26 14:36:38 -0400

    Modfied word scrambler

--- Chapter-08/chapter-08-project/word-scrambler.js
+++ Chapter-08/chapter-08-project/word-scrambler.js
@@ -1,7 +1,7 @@
 // Create a function that returns a value of a word and scrambles the letter order with Math.random():
 
 // Create a string that will hold a word value of your choice
-let str = "March Break";
+let str = "scrambles";
 
 // Create a function that can intake a parameter of the string word value
 
@@ -14,6 +14,7 @@ function scramble(val) {
     // variable that will randomly select one letter using its index value
 
     let randomNum = Math.floor(Math.random() * val.length);
+    console.log(randomNum);
     // Add the new letter to the new string and remove it from the original string.
     temp += val[randomNum];
     // Using console.log() output the newly constructed string from the random letters
