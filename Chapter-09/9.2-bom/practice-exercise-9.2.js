eb589cf1da87f2e24b0da4b947bf1848056e9631
masatosotoma <masatosotoma@gmail.com>
2022-03-27 19:31:30 -0400

    To check the location objects (protocol and href)

---
+++ Chapter-09/9.2-bom/practice-exercise-9.2.js
@@ -0,0 +1,10 @@
+/*
+Travel through the window object to get to the location object, then output the values
+of the protocol and href properties of the current file, into the console.
+*/
+
+console.dir(location.protocol);
+// it navigates to http:
+
+console.dir(location.href);
+// it navigates to https://www.yahoo.co.jp/
