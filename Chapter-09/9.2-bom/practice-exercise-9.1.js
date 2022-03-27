a8d478e36835f15833608d9b2a51a29dbb8bbeb6
masatosotoma <masatosotoma@gmail.com>
2022-03-27 19:06:02 -0400

    Inspected yahoo.co.jp website

---
+++ Chapter-09/9.2-bom/practice-exercise-9.1.js
@@ -0,0 +1,15 @@
+// Go to the website you were just viewing and execute the command below
+
+console.dir(window);
+
+/* find the document object that is nested within the window object
+Under the root of the window object in the console navigate down to the object that is named document
+find the height and width (in pixels) of your window. You return the inner and outer window
+
+innerHeight: 937
+innerWidth: 1270
+
+outerHeight: 1038
+outerWidth: 1918
+
+*/
