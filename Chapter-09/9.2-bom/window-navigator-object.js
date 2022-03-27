2e1bed0ca7875ae69c4100b6735bc4dfade65381
masatosotoma <masatosotoma@gmail.com>
2022-03-27 19:20:46 -0400

    Explained how to see the window navigator object

---
+++ Chapter-09/9.2-bom/window-navigator-object.js
@@ -0,0 +1,9 @@
+/*
+navigator property = an object of the window object which we specify with the dot in between
+we access these window objects in the same way we do any other object
+As navigator is also globally available, we can also access this without window in front of it with the command below
+*/
+
+console.dir(navigator);
+
+// basically the information and the page control of the URL 
