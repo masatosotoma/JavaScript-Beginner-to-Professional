489a82d0ef3abf76d9b9664d3c9b70ff3da6c5d4
masatosotoma <masatosotoma@gmail.com>
2022-05-27 21:15:18 -0400

    Explained breakpoints

---
+++ Chapter-12/12.6-breakpoints.js
@@ -0,0 +1,18 @@
+//A more professional way to go about debugging is to use breakpoints
+
+/*
+Sources panel = in Chrome, You click on the line before your code
+=> A dot or arrow will appear. When your application is running, 
+it will pause at this point to give you the opportunity to inspect the values of variables 
+and walk through thecode line by line from there. 
+*/
+
+/*
+How to use breakpoints in Chrome
+=> In Chrome,go to the Sources tab of the **Inspect** panel
+=> Select the file you want to set a breakpoint in
+=> Click on the line number and it sets the breakpoint
+=> Try to trigger the line of code, and when it gets triggered, it pauses
+
+On the very right of the screen I can inspect all the variables and values
+*/
