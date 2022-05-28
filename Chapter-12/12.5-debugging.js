a800473ccef270c946e1a03597aa5a72a70597e6
masatosotoma <masatosotoma@gmail.com>
2022-05-27 21:04:56 -0400

    Explained debugging

---
+++ Chapter-12/12.5-debugging.js
@@ -0,0 +1,9 @@
+/*
+If you are using JavaScript in the browser and it is notbehaving as you would expect, 
+step 1 is always to open the console in the browser.
+Often it will contain errors that can help you further.
+*/
+
+/*If that doesn't solve it, you can **log to the console** in every step of your code, and also log the variables.*/ 
+
+/*Using console.log() during development to see what's happening is rather common.*/
