a6151db96404a18ae0ac643e26513f33db073a9d
masatosotoma <masatosotoma@gmail.com>
2022-06-09 19:09:52 -0400

    Explained concurrency

---
+++ Chapter-13/13.1-concurrency.js
@@ -0,0 +1,8 @@
+/*
+Concurrency = whenever things are happening "at the same time" or in parallel
+we can do a call to some API and not wait for the reply but do something useful in the meantime instead. 
+We can do so using the concept of concurrency.
+
+There are three strategies in JavaScript that you'll need to know of when working
+with concurrency: callbacks, Promises, and the async and await keywords
+*/
