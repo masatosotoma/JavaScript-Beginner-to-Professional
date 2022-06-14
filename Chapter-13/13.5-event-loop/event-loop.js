ea9b63bfb1b5b693c390ef6dd264430982ed74cc
masatosotoma <masatosotoma@gmail.com>
2022-06-13 21:51:55 -0400

    Explained Event loop

---
+++ Chapter-13/13.5-event-loop/event-loop.js
@@ -0,0 +1,10 @@
+/*
+
+JavaScript is a single-threaded language. A thread in this context means a path of execution. 
+If there is only a single path, this means that tasks will have to wait for one another and only one thing can happen at a time.
+
+Event loop = The single executor. a process that executes the actual work. Even though JavaScript is single-threaded, 
+it doesn't mean that it cannot outsource some tasks and wait for them to come back. 
+This is exactly how JavaScript manages to do things in a multithreaded manner
+
+*/
