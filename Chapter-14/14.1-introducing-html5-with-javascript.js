d878ac9bf456c91879e0318ac5bbd4a5073c4739
masatosotoma <masatosotoma@gmail.com>
2022-06-16 21:01:28 -0400

    Introduction of HTML5

---
+++ Chapter-14/14.1-introducing-html5-with-javascript.js
@@ -0,0 +1,13 @@
+/*
+HTML5 is formally a version of HTML.
+When you read HTML5 in a job description, it often means more than just HTML. 
+Usually, the combination of HTML5 with JavaScript, CSS, JSON, and others is included here as well.
+
+Since HTML5 the structure of our page has improved. We have new elements such as <header>, <nav>, and <article>
+
+we can play videos with the <video> element, which means we no longer need Flash since HTML5
+we can work with the <canvas> element to create visuals on the page or to represent visuals such as animation, graphs, and others
+
+Some things that had to be done with JavaScript in the past can now be done solely with HTML, 
+such as adding video and audio to a webpage.
+*/
