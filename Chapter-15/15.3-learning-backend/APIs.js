ec864513b28e9282587fa2e4acd58eee2f8d86c4
masatosotoma <masatosotoma@gmail.com>
2022-07-16 19:50:26 -0400

    Explained APIs

---
+++ Chapter-15/15.3-learning-backend/APIs.js
@@ -0,0 +1,13 @@
+/*
+API (Application Programming Interface) is essentially an interface for code,
+written with more code. A request can be made to an API using (for example) a URL.
+This will trigger a certain piece of code and this piece of code will give a certain response back.
+
+Whenever a user has filled out all the fields and clicks on Submit booking, 
+the API will get triggered by calling the URL and sending all the data that the user has entered to that endpoint (a specific URL)
+
+APIs work with Hypertext Transfer Protocol (HTTP) calls. 
+HTTP is just a protocol for communication between two parties: a client and a server, 
+or a server and another server (in which the requesting server acts like the client)
+*/
+
