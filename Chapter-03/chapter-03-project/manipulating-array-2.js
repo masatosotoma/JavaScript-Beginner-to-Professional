2cdee2f4241ae01879e6ce1afdd724f4d212a2d2
masatosotoma <masatosotoma@gmail.com>
2022-08-04 22:00:51 -0400

    Reviewed Chapter 3 project

---
+++ Chapter-03/chapter-03-project/manipulating-array-2.js
@@ -0,0 +1,33 @@
+const theList = [
+  "Laurence",
+  "Svekis",
+  true,
+  "35",
+  null,
+  undefined,
+  { test: "one", score: 55 },[("one", "two")],
+];
+
+//Remove the first itme and the last itme
+
+theList.shift() //Delete the first element
+console.log(theList);
+
+theList.pop(); //Delete the last element
+console.log(theList);
+
+//Add 'FIRST' to the start of the array
+theList.splice(0,0,'FIRST')
+console.log(theList);
+
+//Assign 'hello world' to the fourth index value
+theList.splice(3,1,'hello world');
+console.log(theList);
+
+//Assign 'MIDDLE' to the third index value
+theList.splice(2,1,'MIDDLE');
+console.log(theList);
+
+//Add LAST to the last position in the array as well as last 3 index values
+theList.splice(4,3,'LAST');
+console.log(theList);
