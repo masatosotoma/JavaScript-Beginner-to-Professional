e748b44063bdcf8dc96227a14238f132e3078bb1
masatosotoma <masatosotoma@gmail.com>
2022-09-26 22:52:18 -0400

    Building item price calculator

---
+++ Chapter-07/chapter-07-project/menu-items-price-calculator-2.js
@@ -0,0 +1,7 @@
+class menu {
+  //Create a class that contains the prices of two menu items as private field declarations.
+  constructor(item1, item2) {
+    this.item1 = item1;
+    this.item2 = item2;
+  }
+}
