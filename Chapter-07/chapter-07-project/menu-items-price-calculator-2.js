65a72f52becd469a79665f7674200bcd31e669d1
masatosotoma <masatosotoma@gmail.com>
2022-09-27 23:07:25 -0400

    Revised the construcor/class

--- Chapter-07/chapter-07-project/menu-items-price-calculator-2.js
+++ Chapter-07/chapter-07-project/menu-items-price-calculator-2.js
@@ -1,7 +1,26 @@
 class menu {
   //Create a class that contains the prices of two menu items as private field declarations.
-  constructor(item1, item2) {
-    this.item1 = item1;
-    this.item2 = item2;
+  constructor(numOfItem1, numOfItem2, costOfItem1, costOfItem2) {
+    this.numOfItem1 = numOfItem1;
+    this.numOfItem2 = numOfItem2;
+    this.costOfItem1 = costOfItem1;
+    this.costOfItem2 = costOfItem2;
+  }
+  calculateTotalCost() {
+    return (
+      this.numOfItem1 * this.costOfItem1 + this.numOfItem2 * this.costOfItem2
+    );
+  }
+  get total() {
+    return this.calculateTotalCost();
   }
 }
+
+let red = new menu(20, 10, 100, 150);
+let blue = new menu(200, 150, 11, 33);
+let green = new menu(125, 248, 18, 62);
+
+console.log(red.total);
+console.log(blue.total);
+console.log(green.total);
+console.log(red.total + blue.total + green.total);
