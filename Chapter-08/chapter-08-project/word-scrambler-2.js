74a9eb1a6009876e85bbcf3b5c67e818de5e6e44
masatosotoma <masatosotoma@gmail.com>
2022-10-02 21:26:24 -0400

    Re-trying to create a word scrambler

---
+++ Chapter-08/chapter-08-project/word-scrambler-2.js
@@ -0,0 +1,12 @@
+//Create a function that returns a value of a word and scrambles the letter order with Math.random():
+
+//1.Create a string that will hold a word value of your choice.
+let answerWord = "CANADA";
+//2.Create a function that can intake a parameter of the string word value.
+function takeString(word) {
+  for (i = 0; i <= word.length; i++) {
+    console.log(word.charAt(i) + " ");
+  }
+}
+
+takeString(answerWord);
