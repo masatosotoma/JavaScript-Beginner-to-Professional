27ff4f498378119446945682dd5afeb651198d2d
masatosotoma <masatosotoma@gmail.com>
2022-10-09 15:50:09 -0400

    Updated the name of the argument

--- Chapter-06/chapter-06-project/recursive-function-2.js
+++ Chapter-06/chapter-06-project/recursive-function-2.js
@@ -2,10 +2,11 @@
 Invoke the function with differentstart numbers as the arguments that are passed into the function. 
 The function should run until the value is greater than 10.*/
 
-function getRecursive(nr) {
-  console.log(nr);
-  if (nr < 10) {
-    getRecursive(++nr);
+function getRecursive(num) {
+  console.log(num);
+  if (num < 10) {
+    getRecursive(++num);
   }
+  console.log(num);
 }
 getRecursive(0);
