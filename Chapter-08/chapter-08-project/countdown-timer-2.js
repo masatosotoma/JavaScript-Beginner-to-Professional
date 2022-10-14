ee4641993bf0e93ec05619f6c565e8f497ffcb86
masatosotoma <masatosotoma@gmail.com>
2022-10-13 23:48:27 -0400

    Added Date.parse for calculate how much time left

--- Chapter-08/chapter-08-project/countdown-timer-2.js
+++ Chapter-08/chapter-08-project/countdown-timer-2.js
@@ -1,6 +1,15 @@
-
-//1. Create a string that will hold a word value of your choice
+//Create an end date that you want to count down to. Format it in a date type format within a string.
 
 let myBirthday = new Date("Sun Feb 05 2023 0:00:00 GMT-4");
 
 console.log(myBirthday);
+
+//Create a countdown function that will parse the endDate() and subtract the current date from that end date.
+function countDownTime() {
+  let timeLeft = Date.parse(myBirthday);
+  let endTime = endDate(myBirthday, timeLeft);
+  console.log(endTime);
+  console.log(timeLeft);
+}
+
+countDownTime();
