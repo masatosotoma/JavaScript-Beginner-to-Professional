fe187e22ec08654c1ed2473801db1a7fd8a75224
masatosotoma <masatosotoma@gmail.com>
2022-11-06 16:30:49 -0500

    Corrected logic for the scrambled array

--- Chapter-08/chapter-08-project/word-scrambler-2.js
+++ Chapter-08/chapter-08-project/word-scrambler-2.js
@@ -4,31 +4,28 @@
 let answerWord = "Toronto";
 //Create a function that can intake a parameter of the string word value.
 function scrambleWord(word) {
-  let defaultArray = []; //the default array for putting word
-  let result = []; // Created the result array
-  for (i = 0; i <= word.length; i++) {
-    defaultArray.push(word.charAt(i)); //Put the word to the default array
-  }
+  let defaultArray = [...word]; //the default array for putting word
+  // for (i = 0; i <= word.length; i++) {
+  //   defaultArray.push(word.charAt(i)); //Put the word to the default array
+  // }
+
+  // [...word].forEach((character) => {
+  //   defaultArray.push(character);
+  // });
   console.log(defaultArray); //Check if the word is in the array
 
   let scrambled = []; // Created the array for the scrambled word
   for (x = 0; x < word.length; x++) {
     //Used the random number method to get the random# of for the index
-    //In order to get the dupulicated letters, set up the x100 of word.length 
-    randomNum = Math.floor(Math.random() * (word.length * 100)); 
+    //In order to get the dupulicated letters, set up the x100 of word.length
+    let randomNum = Math.floor(Math.random() * (word.length - x));
     scrambled.push(defaultArray[randomNum]);
-    if (!scrambled[randomNum]) {
-      scrambled[randomNum] = word[x];
-    } else {
-      i--;
-    }
+    delete defaultArray[randomNum];
+    let filteredArray = defaultArray.filter((ele) => ele !== undefined);
+    defaultArray = filteredArray;
   }
   // To remove undefined values from the scrambled array
-  scrambled.join("");
-  result = scrambled.filter(function (ele) {
-    return ele !== undefined;
-  });
-  console.log(result);
+  console.log(scrambled);
 }
 
 scrambleWord(answerWord);
