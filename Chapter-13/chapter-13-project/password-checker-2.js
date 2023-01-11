1a84117cf04f95cbe60bbbeaf22331be8b66e6d2
masatosotoma <masatosotoma@gmail.com>
2023-01-10 21:27:53 -0500

    Corrected pw checker functions and promise

--- Chapter-13/chapter-13-project/password-checker-2.js
+++ Chapter-13/chapter-13-project/password-checker-2.js
@@ -5,36 +5,32 @@ let allowed = ["yuki", "masa", "73 Blue Eagle"];
 /*Create a login function that will check if the argument is a value that is included in the passwords array. 
 You can use indexof() or the includes() method to check the array for a value and return a Boolean value of the result.*/
 
-function logIn(password) {
-  if (password.include(allowed)) {
-    return true;
-  } else {
-    return false;
-  }
+function pwchecker(password) {
+  return allowed.include(password);
 }
 
 /*Add a function that returns a Promise. Using resolve and reject, return a JavaScript object with 
 the Boolean of true or false to indicate the password validity status.*/
-
-let promise = new Promise(function (resolve, reject) {
-  if (logIn == true) {
-    resolve(password);
-  } else if (logIn == false) {
-    reject("The password is invalid");
-  } else {
-    console.log("Need to enter correctly");
-  }
-});
-
+function logIn(pw) {
+  return new Promise(function (resolve, reject) {
+    if (pwchecker(pw)) {
+      resolve({ status: true });
+    } else {
+      reject({ status: false });
+    }
+  });
+}
 /*Create a function that checks the password, sending it to the login function, 
 and using then() and catch(), outputs the result of either the rejected password or the resolved password. */
-promise.then(
-  function (value) {
-    console.log("Success : " + value);
-  },
-  function (error) {
-    console.log("Error : " + error);
-  }
-);
 
+function checker(password) {
+  logIn(password)
+    .then((token) => {
+      console.log("Success : " + token);
+    })
+
+    .catch((value) => {
+      console.log("Reject : " + value);
+    });
+}
 //Send several passwords, some within the array, others not, to the checker function.
