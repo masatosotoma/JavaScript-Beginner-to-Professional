45f9b44011c39f864f34b5838fff52a0bfbaab5f
masatosotoma <masatosotoma@gmail.com>
2023-02-12 13:06:19 -0500

    Finished fetching data of array with Json

--- Chapter-15/chapter-15-project/working-with-json-2.js
+++ Chapter-15/chapter-15-project/working-with-json-2.js
@@ -1,34 +1,43 @@
 //Create a file with the extension JSON name it people.json.
-let url = "people.json"
+let url = "people.json";
 
+/*Create an HTML file and add a JavaScript file. Within the JavaScript file use people.json as the URL. 
+Using fetch connect to the URL and retrieve the data. */
 
 fetch(url)
-.then((response) => response.json())
-.then((data)=>{
+  /*Since this is a JSON-formatted file, 
+once you get the response data it can be formatted into JSON using the .json() method in fetch*/
+  .then((response) => response.json())
+  .then((data) => {
+    //Output the full contents of the data into the console.
+    console.log(data);
     data.forEach((person) => {
-        
+      /*Using foreach, loop through the items in the data and output the values into the console. 
+You can use a template literal and output each value.*/
+      console.log(`${person.first} + ${person.last} + ${person.topic}`);
     });
-});
+ß  });
 
 /*Within people.json create an array that contains multiple objects. 
 Each item in the array should be an object that has the same structure, 
 using first, last, and topic as the property names
 Make sure you use double quotes around the property names and values as this is the proper JSON syntax.*/
-[
-    {
-        first:"Masato",
-        last:"Sotoma",
-        topic:"travel"
-    },
-    {
-        first:"Zhang",
-        last:"Claire",
-        topic:"insurance"
-    },
-    {
-        first:"Rankine",
-        last:"Xuen",
-        topic:"marketing"
-    }
+//Add three or more entries into the array using the same object structure for each item
 
-]
+[
+  {
+    first: "Masato",
+    last: "Sotoma",
+    topic: "travel",
+  },
+  {
+    first: "Zhang",
+    last: "Claire",
+    topic: "insurance",
+  },
+  {
+    first: "Rankine",
+    last: "Xuen",
+    topic: "marketing",
+  },
+];
