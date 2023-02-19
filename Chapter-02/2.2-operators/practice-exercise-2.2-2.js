b4e9bf2547ba979d7dac0d218cdd70ab03b8d78b
masatosotoma <masatosotoma@gmail.com>
2023-02-19 07:52:47 -0500

    Practiced operators and log to the console

---
+++ Chapter-02/2.2-operators/practice-exercise-2.2-2.js
@@ -0,0 +1,12 @@
+/*Create a variable for your name, another one for your age, 
+and another one for whether you can code JavaScript or not.
+Log to the console the following sentence, where name, 
+age and true/false are variables: */
+
+let myName = "Masato";
+let myAge = 33;
+let js = true;
+
+console.log(
+  `Hello, my name is ${myName}. I am ${myAge} years old and I can code JavaScript: ${js}.`
+);
