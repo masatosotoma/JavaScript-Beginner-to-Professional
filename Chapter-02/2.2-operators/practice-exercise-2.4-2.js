70f0253ec6735559d6c85f11b4f957e222307be5
masatosotoma <masatosotoma@gmail.com>
2023-02-23 21:56:56 -0500

    Completed Practice Exe 2.4 2nd time

---
+++ Chapter-02/2.2-operators/practice-exercise-2.4-2.js
@@ -0,0 +1,20 @@
+/*Create variables for three numbers: a, b, and c. 
+Update these variables with the following actions using the assignment operators: */
+
+let a = 23;
+let b = 17;
+let c = 73;
+
+//Add b to a
+a = a + b;
+//Divide a by c
+a = a / c;
+
+//Replace the value of c with the modulus of c and b
+
+c = c % b;
+
+//Print all three numbers to the console
+console.log(a);
+console.log(b);
+console.log(c);
