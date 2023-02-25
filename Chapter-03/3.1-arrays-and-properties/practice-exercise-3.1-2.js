dd90bd8e0f711ef99d2418d27340f371119a0076
masatosotoma <masatosotoma@gmail.com>
2023-02-24 23:10:33 -0500

    Completed Practice exercise 3.1 2nd time

---
+++ Chapter-03/3.1-arrays-and-properties/practice-exercise-3.1-2.js
@@ -0,0 +1,11 @@
+//1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
+let arr = ["Milk", "Bread", "Apples"];
+
+//2. Check your list length in the console.
+console.log(arr.length);
+
+//3. Update "Bread" to "Bananas."
+arr[1] = "Bananas";
+
+//4. Output your entire list to the console.
+console.log(arr);
