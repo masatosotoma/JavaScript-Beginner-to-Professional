e5931a56977f0e5dd2365fc62b522255b80a945e
masatosotoma <masatosotoma@gmail.com>
2023-03-04 11:35:09 -0500

    Completed Practice exercise 5.2 2nd time

---
+++ Chapter-05/5.2-do-while-loop/practice-exercise-5.2-2.js
@@ -0,0 +1,12 @@
+// 1. Set the starting counter to 0
+let startCount = 0;
+
+// 2. Create a variable, step, to increase your counter by
+let step = 10;
+
+// 3. Add a do while loop, printing the counter to the console and incrementing it by the step amount each loop
+// 4. Continue to loop until the counter is equal to 100 or more than 100
+do {
+  console.log(startCount);
+  startCount += step;
+} while ((startCount <= 100));
