4ce7a402d113db2f28dd483af0b35e99ba84c518
masatosotoma <masatosotoma@gmail.com>
2023-03-10 20:56:26 -0500

    Completed practice exercise 6.1 2nd time

---
+++ Chapter-06/6.1-basic-functions/practice-exercise-6.1-2.js
@@ -0,0 +1,15 @@
+// 1. Create a function that takes two parameters, adds the parameters together, and returns the result.
+function test(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+// 2. Set up two different variables with two different values.
+let a = 14;
+let b = 235;
+
+// 3. Use your function on the two variables, and output the result using console.log.
+console.log(test(a, b));
+
+// 4. Create a second call to the function using two more numbers as arguments sent to the function.
+console.log(test(5509, 3327));
