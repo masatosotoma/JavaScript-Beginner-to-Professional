09d13b456f6b11a2596cb66e8d458a179cf2545e
masatosotoma <masatosotoma@gmail.com>
2023-03-13 21:29:59 -0400

    Completed practice exercise 6.8 2nd time

---
+++ Chapter-06/6.7-anonymous-functions/practice-exercise-6.8-2.js
@@ -0,0 +1,14 @@
+/*1. Set a variable name and assign a function to it. 
+Create a function expression with one parameter that outputs a provided argument to the console.*/
+let func = function (para) {
+  console.log(para);
+};
+func("hello");
+
+// 3. Create the same function as a normal function declaration.
+
+function funcDemo(arg) {
+  console.log(arg);
+}
+
+funcDemo("hi");
