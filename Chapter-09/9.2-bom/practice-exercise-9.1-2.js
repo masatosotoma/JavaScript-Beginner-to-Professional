368ad1a9e72df64e401d7f0097a31e70dc7d8458
masatosotoma <masatosotoma@gmail.com>
2023-03-22 21:30:01 -0400

    Checked the google website for practice exercise

---
+++ Chapter-09/9.2-bom/practice-exercise-9.1-2.js
@@ -0,0 +1,7 @@
+/*1. Go back to the website you were just viewing and execute the command console.dir(window). */
+console.dir(window);
+
+/*2. Can you find the document object that is nested within the window object? 
+Under the root of the window object in the console, 
+you can navigate down to the object that is named document. */
+
